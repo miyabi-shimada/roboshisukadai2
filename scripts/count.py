e94ce78f1fd21093193bdd942dd0e9cef9367c18
miyabi-shimada <54259119+miyabi-shimada@users.noreply.github.com>
2021-01-11 20:59:33 +0900

    Update count.py

--- scripts/count.py
+++ scripts/count.py
@@ -2,6 +2,7 @@ PDX-License-Identifier: GPL-3.0-only
     '''
         Copyright (C) 2020 Haruki Shimotori and Ryuichi Ueda.All right reserved.
     '''
+#!/usr/bin/env python3
     
 import rospy
 from std_msgs.msg import String
