0fec12443bd83a79a0f887a46cd24d34ceaecd72
miyabi-shimada <54259119+miyabi-shimada@users.noreply.github.com>
2021-01-11 21:00:03 +0900

    Update twice.py

--- scripts/twice.py
+++ scripts/twice.py
@@ -3,6 +3,8 @@ PDX-License-Identifier: GPL-3.0-only
     Copyright (C) 2020 Haruki Shimotori and Ryuichi Ueda.All rit reserved.
  '''
 
+#!/usr/bin/env python3
+
 import rospy
 from std_msgs.msg import String
 
