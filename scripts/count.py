335d6ce81364aae1eaa516e378a66a64450b084c
miyabi-shimada <54259119+miyabi-shimada@users.noreply.github.com>
2021-01-12 12:21:26 +0900

    Update count.py

--- scripts/count.py
+++ scripts/count.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
-    
+#!/usr/bin/env python3 
 import rospy
 from std_msgs.msg import String
 
-rospy.init_node('count')                                # ノード名「count」に設定
-pub = rospy.Publisher('count_up', String, queue_size=1)  # パブリッシャ「count_up」を作成
-rate = rospy.Rate(10)    # 10Hzで実行
+rospy.init_node('count') # ノード名「count」に設定
+pub = rospy.Publisher('count_up', String, queue_size=1) # パブリッシャ「count_up」を作成
+rate = rospy.Rate(10) # 10Hzで実行
 
 while not rospy.is_shutdown():
-    rospy.loginfo("茶々天使♡♥♡♥♡♥♡♥♡♥♡♥")
+    rospy.loginfo("茶々天使♡ ♥ ♡ ♥ ♡ ♥ ♡ ♥ ♡ ♥ ♡ ♥")
     pub.publish()
     rate.sleep()
